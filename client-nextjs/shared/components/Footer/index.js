0c640730ca7dbda66a410c8c2e91768692e13cdc
Sirima Insorn <sirimainsorn@gmail.com>
2022-04-20 19:54:09 +0700

    [feat] add component header and footer

--- client-nextjs/shared/components/Footer/index.js
+++ client-nextjs/shared/components/Footer/index.js
@@ -1,8 +1,29 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-export default function Footer(props) {
-  return <div>Footer</div>;
+export default function Footer() {
+  return (
+    <div className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        {/* menu footer */}
+        <div className="py-6 lg:py-8 grid grid-cols-4 gap-4 text-gray-400">
+          <div>
+            <h2 className="font-bold">CONTACT US</h2>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h2 className="font-bold">INFORMATION</h2>
+            <a className="text-sm">About Us</a>
+            <a className="text-sm">Privacy Policy</a>
+            <a className="text-sm">Contact Us</a>
+          </div>
+        </div>
+        {/* copy right */}
+        <div className="flex justify-between py-4 lg:py-6">
+          <div>
+            <h4 className="text-gray-400 text-sm">2020 Workflow</h4>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-Footer.propTypes = {};
--- client-nextjs/shared/components/Layout/LayoutDefault.js
+++ client-nextjs/shared/components/Layout/LayoutDefault.js
@@ -1,5 +1,15 @@
 import React from "react";
+import Footer from "../Footer";
+import Header from "../Header";
 
-export default function LayoutDefault() {
-  return <div>LayoutDefault</div>;
+export default function LayoutDefault(props) {
+  const { children } = props;
+
+  return (
+    <div className="flex flex-col min-h-screen font-default">
+      <Header />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
 }
